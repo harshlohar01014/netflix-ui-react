ebd6c250a0a0c621e587b3d01f65b4431b0893ea
harshlohar01014 <160117322+harshlohar01014@users.noreply.github.com>
2024-07-15 14:07:04 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./app.scss"
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+
+const App = () => {
+  return(
+  <>
+  
+  <Register/>
+  <Watch/>
+  <Login/>
+  <Home/>
+  </>
+ 
+  )
+  
+};
+
+export default App;
